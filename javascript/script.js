24e01dfbc6e2c2a9e5d1d74b886b10dda63653da
Hugo Lomba <hmlomba@gmail.com>
2022-07-21 22:28:15 -0300

    add motion to ufo

--- javascript/script.js
+++ javascript/script.js
@@ -4,12 +4,7 @@ const canvas = document.getElementById("canvas");
 const startScreen = document.getElementById("start-screen");
 const startBtn = document.getElementById("start-btn");
 
-// Troca da tela de início para o canvas ao apaertar o botão Start
-startBtn.addEventListener("click", () => {
-  startScreen.style.display = "none";
-  canvas.style.display = "inline";
-  ufo.draw(ufo.img, ufo.x, ufo.y);
-});
+const ctx = canvas.getContext("2d");
 
 // Classes
 class Component {
@@ -20,22 +15,79 @@ class Component {
     this.height = height;
     this.x = x;
     this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
   }
 
   draw() {
-    this.img.addEventListener("load", () =>
-      ctx.drawImage(this.img, this.width, this.height, this.x, this.y)
-    );
-    console.log("funcionou");
+    // this.img.addEventListener("load", () =>
+    ctx.drawImage(this.img, this.x, this.y);
+    // );
+  }
+
+  clear() {
+    ctx.clearRect(this.x, this.y, this.width, this.height);
+  }
+
+  newPos() {
+    this.x += this.speedX;
+    this.y += this.speedY;
   }
 }
+
 // objetos
 
-const ufo = new Component("./img/ufo2.png", 100, 100, 136, 65);
+const ufo = new Component("./img/ufo2.png", 136, 65, 100, 100);
+
+// Troca da tela de início para o canvas ao apaertar o botão Start
+
+startBtn.addEventListener("click", () => {
+  startScreen.style.display = "none";
+  canvas.style.display = "inline";
+  ufo.draw();
+});
 
 // manipulação do Canvas
 
-const ctx = canvas.getContext("2d");
-ufo.draw(ufo.img, ufo.x, ufo.y);
+// ufo.draw();
 
 // englobar tudo no add event listener em todas as imagens
+
+// movimento ao pressionar
+
+document.addEventListener("keydown", (e) => {
+  const key = e.code;
+  if (key === "ArrowUp") {
+    ufo.speedY -= 1;
+    console.log("Up");
+    ufo.clear();
+    ufo.newPos();
+    ufo.draw();
+  }
+  if (key === "ArrowDown") {
+    ufo.speedY += 1;
+    console.log("down");
+    ufo.clear();
+    ufo.newPos();
+    ufo.draw();
+  }
+  if (key === "ArrowLeft") {
+    ufo.speedX -= 1;
+    console.log("left");
+    ufo.clear();
+    ufo.newPos();
+    ufo.draw();
+  }
+  if (key === "ArrowRight") {
+    ufo.speedX += 1;
+    console.log("right");
+    ufo.clear();
+    ufo.newPos();
+    ufo.draw();
+  }
+});
+
+document.addEventListener("keyup", () => {
+  ufo.speedX = 0;
+  ufo.speedY = 0;
+});
